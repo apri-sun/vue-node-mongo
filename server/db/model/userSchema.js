74ae87ec227a832875d9c1e7580d69c9386c6036
xiaofan <619307401@qq.com>
2020-02-23 15:00:25 +0800

    密码进行简单的MD5加密

--- server/db/model/userSchema.js
+++ server/db/model/userSchema.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+
 var userSchema = mongoose.Schema({
     us: {type: String, required: true},
     ps: {type: String, required: true},
     sex: {type: Number, default: 0}
-});
+})
 
 var User = mongoose.model('user', userSchema);
 
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const user = require('../db/model/userSchema')
+const MD5 = require('crypto-js/md5')
+
+function encipherment(ps) {
+    return MD5(ps).toString()
+}
 
 router.post('/register', (req, res) => {
     let {us, ps} = req.body
-    console.log(us, ps)
-    user.insertMany({us, ps})
+    // 加密
+    ps = encipherment(ps)
+    user.find({us}, (err, docs) => {
+        if(docs.length === 0) {
+            user.insertMany({us, ps})
+            return res.send({err: 0, msg: '注册成功'})
+        }
+        return res.send({err: 2, msg: '用户已注册'})
+    })
+})
+
+router.get('/login', (req, res) => {
+    let {us, ps} = req.body
+    ps = encipherment(ps)
+    console.log(ps)
+    user.find({us, ps}, (err, docs) => {
+        if(err) {
+            return res.send({err: 3, msg: '服务器内部错误'})
+        }
+        if(docs.length === 0) { 
+            return res.send({err: 1, msg: '用户名或密码错误'}) 
+        }
+        return res.send({err: 0, msg: '登录成功'})
+    })
 })
 
 module.exports = router
